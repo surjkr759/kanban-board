95c81b4999e9edda805ea0da4ef596c1f3e04241
suraj <surajkr.759@gmail.com>
2024-04-20 23:36:02 +0530

    Edit Delete Board functionality added

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ const init = () => {
 
     document.body.addEventListener('click', (event) => {
         const menu = document.querySelector('.menu');
-        if (menu && !menu.contains(event.target)) {
+        const dialog = document.querySelector('#editBoard');
+        
+        if (menu && !dialog && !menu.contains(event.target)) {
             menu.remove();
         }
     });
@@ -33,22 +35,22 @@ const init = () => {
 const addNewBoard = () => {
     const boardContainer = document.getElementById('board-container');
 
-    const newDialogBox = createNewDialogBox();
+    const newDialogBox = createNewDialogBox('addBoard', 'addBoardDialogBox');
 
-    const dialogBoxHeading = createdialogBoxHeading();
+    const dialogBoxHeading = createdialogBoxHeading('New Board');
     const hr1 = createHR('hr1');
     const boardTitle = setNewBoardTitle();
-    const boardTitleInput = createNewBoardTitleInput();
+    const boardTitleInput = createNewBoardTitleInput('');
     const boardDescription = createBoardDescription();
-    const boardDescriptionInput = createNewBoardDescriptionInput();
+    const boardDescriptionInput = createNewBoardDescriptionInput('');
     const boardColor = createNewBoardColorParentDiv();
     const boardColorsContainer = createBoardColorsContainer();
     setColorsInColorContainer(boardColorsContainer);
     const hr2 = createHR('hr2');
 
     const buttonContainer = createButtonContainer();
-    const cancelButton = createCancelButton();
-    const submitButton = createSubmitButton(boardTitleInput, boardDescriptionInput, newDialogBox);
+    const cancelButton = createCancelButton(newDialogBox);
+    const submitButton = createAndSetSubmitButtonToAddNewBoard(boardTitleInput, boardDescriptionInput, newDialogBox);
 
     buttonContainer.append(cancelButton);
     buttonContainer.append(submitButton);
@@ -90,6 +92,7 @@ const createNewBoard = (boardTitle, boardDescription) => {
     countTasks();
     taskDragListener();
     boardDragListener();
+    threeDotsFun();
 }
 
 const createAndSetBoardTitleDescription = (id, t, desc) => {
@@ -103,8 +106,6 @@ const createAndSetBoardTitleDescription = (id, t, desc) => {
     createBoardTitle(titleContainer, color, title, taskCount);
 
     const threeDotsImgContainer = createThreeDotsImgContainer();
-    const threeDotsImg = createAndSetThreeDotsImg();
-    threeDotsImgContainer.append(threeDotsImg);
 
     parentTitleContainer.append(titleContainer);
     parentTitleContainer.append(threeDotsImgContainer);
@@ -114,7 +115,7 @@ const createAndSetBoardTitleDescription = (id, t, desc) => {
     return header;
 }
 
-const createAndSetThreeDotsImg = () => {
+const createAndSetThreeDotsImg = (id) => {
     const threeDotsImg = createThreeDotsImg();
     threeDotsImg.setAttribute('src', '/Images/three-dots.svg');
     threeDotsImg.setAttribute('alt', 'Del');
@@ -124,7 +125,7 @@ const createAndSetThreeDotsImg = () => {
         const menu = createContextMenu();
 
         // Calculate position for menu
-        calcMenuPos(threeDotsImg, menu);
+        calcMenuPos(threeDotsImg, menu, event);
 
         // Ensure there's only one menu at a time
         const existingMenu = document.querySelector('.menu');
@@ -132,8 +133,8 @@ const createAndSetThreeDotsImg = () => {
             existingMenu.remove();
         }
 
-        document.body.appendChild(menu);
-        // document.body.children[1].children[id].children[0].children[0].children[1].appendChild(menu);
+        // document.body.appendChild(menu);
+        document.body.children[1].children[id].children[0].children[0].children[1].appendChild(menu);
     })
     return threeDotsImg;
 }
@@ -155,9 +156,53 @@ const createEditOption = () => {
     const editOption = document.createElement('li');
     editOption.setAttribute('class', 'edit');
     editOption.textContent = 'Edit';
-    editOption.addEventListener('click', () => {
+    editOption.addEventListener('click', (event) => {
+        event.stopPropagation();
         // Handle edit action
         console.log('Edit option clicked');
+        const boardContainer = document.getElementById('board-container');
+
+        const newDialogBox = createNewDialogBox('editBoard', 'editBoardDialogBox');
+        const dialogBoxHeading = createdialogBoxHeading('Edit Board');
+        const hr1 = createHR('hr3');
+        const boardTitle = setNewBoardTitle();
+        const prevBoardTitleVal = event.target.closest('#parentTitleContainer').children[0].children[1].innerHTML;
+        // console.log(prevBoardTitleVal);
+        const boardTitleInput = createNewBoardTitleInput(prevBoardTitleVal);
+        const boardDescription = createBoardDescription();
+        const prevBoardDescVal = event.target.closest('.header').children[1].innerHTML;
+        // console.log(prevBoardDescVal);
+        const boardDescriptionInput = createNewBoardDescriptionInput(prevBoardDescVal);
+        // const boardColor = createNewBoardColorParentDiv();
+        // const boardColorsContainer = createBoardColorsContainer();
+        
+        // const prevSelectedColor = event.target.closest('#parentTitleContainer').children[0].children[0].style.backgroundColor;
+        // setColorsInColorContainer(boardColorsContainer, prevSelectedColor);
+
+        const hr2 = createHR('hr4');
+
+        const buttonContainer = createButtonContainer();
+        const cancelButton = createCancelButton(newDialogBox);
+        const submitButton = createAndSetSubmitButtonToModifyBoardTitle(boardTitleInput, boardDescriptionInput, newDialogBox, newBoardColorId, event);
+
+        buttonContainer.append(cancelButton);
+        buttonContainer.append(submitButton);
+        
+        newDialogBox.append(dialogBoxHeading);
+        newDialogBox.append(hr1);
+        newDialogBox.append(boardTitle);
+        newDialogBox.append(boardTitleInput);
+        newDialogBox.append(boardDescription);
+        newDialogBox.append(boardDescriptionInput);
+        // newDialogBox.append(boardColor);
+        // newDialogBox.append(boardColorsContainer);
+        newDialogBox.append(hr2);
+        newDialogBox.append(buttonContainer);
+    
+    
+        boardContainer.append(newDialogBox);
+    
+        newDialogBox.showModal();
     });
     return editOption;
 }
@@ -166,14 +211,77 @@ const createDeleteOption = () => {
     const deleteOption = document.createElement('li');
     deleteOption.setAttribute('class', 'delete');
     deleteOption.textContent = 'Delete';
-    deleteOption.addEventListener('click', () => {
-        // Handle edit action
-        console.log('Delete option clicked');
+    deleteOption.addEventListener('click', (event) => {
+        event.stopPropagation();
+        // Handle delete action
+        const board = event.target.closest('.board');
+        console.log('Board removed: ' + board.getAttribute('id'));
+
+        createWarningDialogBox(board);
+        // board.remove();
+        // rewrite3DotsContId();
+        // threeDotsFun();
     });
     return deleteOption;
 }
 
-const calcMenuPos = (threeDotsImg, menu) => {
+const createWarningDialogBox = (board) => {
+    const boardContainer = document.getElementById('board-container');
+
+    const newDialogBox = createNewDialogBox('warning', 'warningDialogBox');
+    const dialogBoxHeading = createWarningDialogBoxHeading('Are you sure you want to delete ' + board.getAttribute('id') + '?');
+
+    const buttonContainer = createButtonContainer();
+    const noButton = createNoButton(newDialogBox);
+    const yesButton = createYesButton(board, newDialogBox);
+
+    buttonContainer.append(yesButton);
+    buttonContainer.append(noButton);
+    
+    newDialogBox.append(dialogBoxHeading);
+    newDialogBox.append(buttonContainer);
+
+    boardContainer.append(newDialogBox);
+
+    newDialogBox.showModal();
+
+}
+
+const createNoButton = (newDialogBox) => {
+    const noButton = createElement('div');
+    noButton.classList.add('warningDialogBoxButtons');
+    noButton.classList.add('noButton');
+    noButton.setAttribute('id', 'noButton');
+    noButton.innerHTML = 'No';
+    noButton.addEventListener('click', () => newDialogBox.remove());
+    return noButton;
+}
+
+const createYesButton = (board, newDialogBox) => {
+    const yesButton = createElement('div');
+    yesButton.classList.add('warningDialogBoxButtons');
+    // yesButton.classList.add('Button');
+    yesButton.setAttribute('id', 'yesButton');
+    yesButton.innerHTML = 'Yes';
+    yesButton.addEventListener('click', () => {
+        board.remove();
+        rewrite3DotsContId();
+        threeDotsFun();
+        newDialogBox.remove();
+    });
+    return yesButton;
+}
+
+const rewrite3DotsContId = () => {
+    let i=0;
+    const threeDotsImgContainer = document.querySelectorAll('.threeDotsImagecontainer');
+    threeDotsImgContainer.forEach(cont => {
+        cont.setAttribute('id', 'threeDotsImagecontainer'+i);
+        i++;
+    })
+}
+
+const calcMenuPos = (threeDotsImg, menu, event) => {
     const rect = threeDotsImg.getBoundingClientRect();
     menu.style.position = 'fixed';
     menu.style.top = (rect.bottom-13) + 'px';
@@ -189,7 +297,7 @@ const createAddItemSection = () => {
 
 const createEmptyBoard = (id) => {
     const b = createElement('div');
-    b.setAttribute('id', id);
+    b.setAttribute('id', 'board'+id);
     b.setAttribute('class', 'board');
     return b;
 }
@@ -331,17 +439,24 @@ const createNewBoardBtn = () => {
 }
     
 
-const createNewDialogBox = () => {
+const createNewDialogBox = (dialogBox, dialogBoxClass) => {
     const newDialogBox = createElement('dialog');
-    newDialogBox.setAttribute('class', 'addBoardDialogBox');
-    newDialogBox.setAttribute('id', 'addBoardDialogBox');
+    newDialogBox.setAttribute('class', dialogBoxClass);
+    newDialogBox.setAttribute('id', dialogBox);
     return newDialogBox;
 }
     
-const createdialogBoxHeading = () => {
+const createdialogBoxHeading = (heading) => {
     const dialogBoxHeading = createElement('div');
     dialogBoxHeading.setAttribute('class', 'dialogBoxHeading');
-    dialogBoxHeading.innerHTML = 'New Board';
+    dialogBoxHeading.innerHTML = heading;
+    return dialogBoxHeading;
+}
+
+const createWarningDialogBoxHeading = (heading) => {
+    const dialogBoxHeading = createElement('div');
+    dialogBoxHeading.setAttribute('class', 'warningDialogBoxHeading');
+    dialogBoxHeading.innerHTML = heading;
     return dialogBoxHeading;
 }
 
@@ -360,9 +475,11 @@ const setNewBoardTitle = () => {
     return boardTitle;
 }
 
-const createNewBoardTitleInput = () => {
+const createNewBoardTitleInput = (val) => {
     const boardTitleInput = createElement('input');
     boardTitleInput.setAttribute('class', 'dialogBodyInput');
+    boardTitleInput.setAttribute('id', 'dialogBodyInput');
+    boardTitleInput.value = val;
     return boardTitleInput;
 }
 
@@ -374,9 +491,11 @@ const createBoardDescription = () => {
     return boardDescription;
 }
 
-const createNewBoardDescriptionInput = () => {
+const createNewBoardDescriptionInput = (val) => {
     const boardDescriptionInput = createElement('textarea');
     boardDescriptionInput.setAttribute('class', 'dialogBodyInput');
+    boardDescriptionInput.setAttribute('id', 'dialogBodyInput');
+    boardDescriptionInput.value = val;
     return boardDescriptionInput;
 }
 
@@ -392,7 +511,7 @@ const createBoardColorsContainer = () => {
     return boardColorsContainer;
 }
 
-const setColorsInColorContainer = (boardColorsContainer) => {
+const setColorsInColorContainer = (boardColorsContainer, defaultSelection) => {
     for(let i=0; i < sampleData.colors.length; i++) {
         const colorDiv = createElement('div');
         colorDiv.setAttribute('type', 'text');
@@ -402,6 +521,10 @@ const setColorsInColorContainer = (boardColorsContainer) => {
         colorDiv.style.backgroundColor = `${sampleData.bgColors[i]}`;
         colorDiv.addEventListener('click', (e) => fun(e));
 
+        if(defaultSelection !== null && (defaultSelection === colorDiv.style.backgroundColor)) {
+            colorDiv.innerHTML = "&nbsp;✓";
+            newBoardColorId = colorDiv.getAttribute('id');
+        }
         boardColorsContainer.append(colorDiv);
     }
 }
@@ -412,26 +535,24 @@ const createButtonContainer = () => {
     return buttonContainer;
 }
 
-const createCancelButton = () => {
+const createCancelButton = (newDialogBox) => {
     const cancelButton = createElement('div');
     cancelButton.setAttribute('class', 'dialogButton');
     cancelButton.setAttribute('id', 'cancelButton');
     cancelButton.innerHTML = 'Cancel';
-    cancelButton.addEventListener('click', () => newDialogBox.close());
+    cancelButton.addEventListener('click', () => newDialogBox.remove());
     return cancelButton;
 }
 
-const createSubmitButton = (boardTitleInput, boardDescriptionInput, newDialogBox) => {
-    const submitButton = createElement('div');
-    submitButton.setAttribute('class', 'dialogButton');
-    submitButton.setAttribute('id', 'submitButton');
-    submitButton.innerHTML = 'Submit';
+const createAndSetSubmitButtonToAddNewBoard = (boardTitleInput, boardDescriptionInput, newDialogBox) => {
+    const submitButton = createSubmitButton();
+
     submitButton.addEventListener('click', () => {
         if(boardTitleInput.value.replace(/\s+/g,' ').trim() !== '') {
             if(boardDescriptionInput.value.replace(/\s+/g,' ').trim() !== '') {
                 if(newBoardColorId !== null) {
                     createNewBoard(boardTitleInput.value, boardDescriptionInput.value, newBoardColorId);
-                    newDialogBox.close();
+                    newDialogBox.remove();
                 } else {
                     alert('Board color must be selected');
                 }
@@ -450,6 +571,55 @@ const createSubmitButton = (boardTitleInput, boardDescriptionInput, newDialogBox
     return submitButton;
 }
 
+const createAndSetSubmitButtonToModifyBoardTitle = (boardTitleInput, boardDescriptionInput, newDialogBox, id, event) => {
+    const submitButton = createSubmitButton();
+
+    submitButton.addEventListener('click', () => {
+        if(boardTitleInput.value.replace(/\s+/g,' ').trim() !== '') {
+            if(boardDescriptionInput.value.replace(/\s+/g,' ').trim() !== '') {
+                // if(newBoardColorId !== null) {
+                    modifyBoardTitle(boardTitleInput.value, boardDescriptionInput.value, id, event);
+                    // newDialogBox.close();
+                    newDialogBox.remove();
+                    const menu = document.querySelector('.menu');
+                    console.log('Menu: ' + menu);
+                    if (menu) {
+                        menu.remove();
+                    }
+                // } else {
+                //     alert('Board color must be selected');
+                // }
+                
+            } else {
+                alert('Description must not be empty');
+                boardDescriptionInput.value = '';
+            }
+        } else {
+            alert('Title must not be empty');
+            boardTitleInput.value = '';
+        }
+        
+    });
+
+    return submitButton;
+}
+
+const modifyBoardTitle = (tit, desc, id, e) => {
+    e.target.closest('#parentTitleContainer').children[0].children[1].innerHTML = tit;
+    e.target.closest('.header').children[1].innerHTML = desc;
+    console.log('Id: ' + `${sampleData.bgColors[id]}`);
+    e.target.closest('#parentTitleContainer').children[0].children[0].style.backgroundColor = `${sampleData.bgColors[id]}`;
+    e.target.closest('#parentTitleContainer').children[0].children[0].style.border = `2px solid ${sampleData.colors[id]}`;
+}
+
+const createSubmitButton = () => {
+    const submitButton = createElement('div');
+    submitButton.setAttribute('class', 'dialogButton');
+    submitButton.setAttribute('id', 'submitButton');
+    submitButton.innerHTML = 'Submit';
+    return submitButton;
+}
+
 const createElement = (x) => {
     return document.createElement(x);
 }
@@ -464,4 +634,25 @@ const fun = (event) => {
     newBoardColorId = event.target.id;
 }
 
+const threeDotsFun = () => {
+    const threeDotsImgContainer = document.querySelectorAll('.threeDotsImagecontainer');
+    // Ensure all 3 dots are removed beforehand
+    removeExistingThreeDots();
+    threeDotsImgContainer.forEach(container => createThreeDots(container));    
+}
+
+const removeExistingThreeDots = () => {
+    const existingContainers = document.querySelectorAll('.threeDotsImg');
+    existingContainers.forEach(cont => cont.remove());  
+}
+
+const createThreeDots = (container) => {
+    const fullId = container.getAttribute('id');
+    const id = fullId.charAt(fullId.length - 1);
+    const threeDotsImg = createAndSetThreeDotsImg(id);
+    container.append(threeDotsImg);
+}
+
+
 init();
+threeDotsFun();
